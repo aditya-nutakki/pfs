36419ac953d97fa59cf021c2d26ac6b2ecc62c3d
Aditya Nutakki <adityanutakki6250@gmail.com>
2023-10-31 01:14:15 +0530

    text encoder

--- clip/helpers.py
+++ clip/helpers.py
@@ -8,6 +8,8 @@ from collections import OrderedDict
 
 from torchvision.models.vision_transformer import VisionTransformer
 
+from transformers import DistilBertConfig, DistilBertModel, DistilBertTokenizer
+
 
 class ImageEncoder(nn.Module):
     def __init__(self, image_size = 224, patch_size = 16, num_layers = 6, num_heads = 8,
@@ -41,14 +43,32 @@ class ImageEncoder(nn.Module):
 
 
 class TextEncoder(nn.Module):
-    def __init__(self, embedding_dim = 256) -> None:
+    def __init__(self, vocab_size , max_seq_len = 8, num_layers = 4, nhead = 8, d_model = 128) -> None:
         super().__init__()
-        self.embedding_dim = embedding_dim
+        
+        self.max_seq_len = max_seq_len
+        self.nhead = nhead
+        self.vocab_size = vocab_size
+        self.d_model = d_model
+        self.num_layers = num_layers
+
+        self.embedding = nn.Embedding(self.vocab_size, self.d_model)
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model = self.d_model, nhead = self.nhead)
+        # self.model = nn.TransformerEncoder(encoder_layer = self.encoder_layer, num_layers = self.num_layers)
+        self.model = DistilBertModel.from_pretrained("distilbert-base-uncased")
+        self.embedding_proj = nn.Linear(768, self.d_model)
+        self.dropout = nn.Dropout(0.2)
 
 
-    def forward(self, captions):
-        return 0
-
+    def forward(self, x):
+        # captions to be in tensor form
+        x = self.model(x)
+        # print(x.shape)
+        x = x[0][:, 0] # distilbert output
+        # print(x.shape)
+        x = self.dropout(x)
+        x = self.embedding_proj(x)
+        return x
 
 
 
@@ -69,11 +89,19 @@ class CLIPDataset(Dataset):
 
 
 if __name__ == "__main__":
-    enc = ImageEncoder()
+    # enc = ImageEncoder()
+    enc = TextEncoder(vocab_size = 10)
     print(sum([p.numel() for p in enc.parameters()]))
     
     # x = torch.randn(4, 3, 224, 224)
     # y = enc(x)
     # print(y, y.shape)
     # print(torch.min(y), torch.max(y))
-    enc = TextEncoder()
+
+    x = torch.randint(0, 10, (4, 12))
+    print(x)
+    y = enc(x)
+    # hidden_state = y.last_hidden_state
+    # print(hidden_state.shape)
+    print(y, y.shape)
+    # print(y)
